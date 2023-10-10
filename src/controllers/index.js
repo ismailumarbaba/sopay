cb5085f3a997d120737ad829ae0f7cdd5a79b2e5
Ismail Umar <ismailumarbaba@gmail.com>
2023-10-10 02:51:47 +0100

    updated

--- src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-const circle = require('./circle');
-const item = require('./item');
-const feedback = require('./feedback');
-const user = require('./user');
-
-module.exports = {
-  circle, item, feedback, user
-};
